c2d6e6474b33815c328959c5eb9444bf4980d231
sneharavichandran <78321495+sneharavichandran@users.noreply.github.com>
2021-03-30 00:32:44 +0530

    using a different file

---
+++ Assignment4_1/src/Business/Customer/CustomerDirectory.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Customer;
+
+import Business.Employee.Employee;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author harold
+ */
+public class CustomerDirectory {
+     private ArrayList<Customer> customerList;
+
+    public CustomerDirectory() {
+        customerList = new ArrayList();
+    }
+
+    public ArrayList<Customer> getCustomerList() {
+        return customerList;
+    }
+    
+    public Customer createCustomer(String name){
+        Customer customer = new Customer();
+        customer.setName(name);
+        customerList.add(customer);
+        return customer;
+    }
+    
+}
---
+++ Assignment4_1/src/Business/Restaurant/Restaurant.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Restaurant;
+
+/**
+ *
+ * @author harold
+ */
+public class Restaurant {
+    private String name;
+    private int id;
+    private static int count = 1;
+
+    public Restaurant() {
+        id = count;
+        count++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+    
+}
